946c5a956641152a6336b3d45042726a8a59f76e
giangbang <banggiangle2015@gmail.com>
2022-01-28 20:35:12 +0700

    add limit size to cache

--- src/dataset/cache.py
+++ src/dataset/cache.py
@@ -2,8 +2,10 @@ import os
 import numpy as np
 
 class CacheImg:
-	def __init__(self, save_to=None):
+	def __init__(self, save_to=None, limit=100000):
 		self.save_to = save_to
+		self.cache_hit = 0
+		self.limit = limit
 		if save_to is None:
 			cur_dir = os.path.dirname(os.path.realpath(__file__))
 			self.save_to = os.path.join(cur_dir, 'data')
@@ -13,6 +15,7 @@ class CacheImg:
 		self.indices = set(os.listdir(self.save_to))
 		print('Found {} characters in cache folder'.format(len(self.indices)))
 		
+		
 	def add(self, data, id):
 		'''
 		Add new data to cache file
@@ -20,6 +23,8 @@ class CacheImg:
 		'''
 		# if self._getname(id) in self.indices:
 			# return
+		if len(self.indices) >= self.limit:
+			return
 		self.indices.add(self._getname(id))
 		path = os.path.join(self.save_to, self._getname(id))
 		with open(path, 'wb') as f:
@@ -30,6 +35,10 @@ class CacheImg:
 		return self._getname(id) in self.indices
 		
 	def get(self, id):
+		self.cache_hit += 1
+		if (self.cache_hit + 1) % self.limit == 0:
+			print('Cache hit rate reaches {} times'.format(self.cache_hit))
+			
 		im_path = os.path.join(self.save_to, self._getname(id))
 		data = None
 		with open(im_path, 'rb') as f:
