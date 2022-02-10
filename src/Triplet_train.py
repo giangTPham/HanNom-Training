3065d6742a55242c1810c86063e03b62e36a2565
GiangTPham <phatr2000@gmail.com>
2022-02-10 13:34:24 +0700

    no shuffle

--- src/Triplet_train.py
+++ src/Triplet_train.py
@@ -40,16 +40,16 @@ def main(cfg: SimpleNamespace) -> None:
 	)
 	
 	train_dataset = TripletDataset(cfg)
-	print("=====Debug Start=========")
-	print(train_dataset.label_list)
-	print(cfg.data.sample_per_cls)
-	print("=====Debug End=========")
+	# print("=====Debug Start=========")
+	# print(train_dataset.label_list)
+	# print(cfg.data.sample_per_cls)
+	# print("=====Debug End=========")
 	train_sampler = samplers.MPerClassSampler(train_dataset.label_list, cfg.data.sample_per_cls, batch_size=None,
 											  length_before_new_iter=len(train_dataset.label_list))
 	train_dataloader = torch.utils.data.DataLoader(train_dataset,
 												   batch_size=cfg.train.batch_size,
 												   sampler=train_sampler,
-												   shuffle=True,
+												   shuffle=False,
 												   drop_last=False,
 												   pin_memory=True,
 												   num_workers=torch.multiprocessing.cpu_count())
