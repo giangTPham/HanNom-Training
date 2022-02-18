8a9a8a958c2399ff9cef468835f2f88235d017d2
giangbang <banggiangle2015@gmail.com>
2022-02-19 01:50:16 +0700

    fix tab

--- src/dataset/imGen/utils.py
+++ src/dataset/imGen/utils.py
@@ -3,16 +3,16 @@ import requests
 from zipfile import ZipFile
 
 def check_font_extension(font_name):
-	ALLOWED_FONT_EXTENSIONS = ['ttf', 'otf', 'ttc']
-	return font_name.rsplit('.', 1)[-1].lower() in ALLOWED_FONT_EXTENSIONS
+    ALLOWED_FONT_EXTENSIONS = ['ttf', 'otf', 'ttc']
+    return font_name.rsplit('.', 1)[-1].lower() in ALLOWED_FONT_EXTENSIONS
 
 def download_font(font_path):
-	print('Downloading fonts...')
-	link_id = '122Xio9ZiJm2Ulqh6U-S5FAabVHMXi_zQ'
-	save_dir = 'fonts.zip'
-	destination = os.path.join(font_path, save_dir)
-	download_file_from_google_drive(link_id, destination)
-	
+    print('Downloading fonts...')
+    link_id = '122Xio9ZiJm2Ulqh6U-S5FAabVHMXi_zQ'
+    save_dir = 'fonts.zip'
+    destination = os.path.join(font_path, save_dir)
+    download_file_from_google_drive(link_id, destination)
+    
     print('Extracting all the fonts...')
     try:
         with ZipFile(destination, 'r') as zip:
@@ -21,49 +21,49 @@ def download_font(font_path):
     except :
         os.system('unzip {}'.format(destination))    
     
-	os.remove(destination)
-	print('Using fonts from {} to generate training data.'.format(font_path))
+    os.remove(destination)
+    print('Using fonts from {} to generate training data.'.format(font_path))
 
 def check_exist_and_download_fonts(font_path):
-	if not (os.path.exists(font_path)):
-		print('{} not fount!'.format(font_path))
-		os.makedirs(font_path)
-		
-		download_font(font_path)
-	elif len(os.listdir(font_path)) == 0:
-		download_font(font_path)
-		
-		
+    if not (os.path.exists(font_path)):
+        print('{} not fount!'.format(font_path))
+        os.makedirs(font_path)
+        
+        download_font(font_path)
+    elif len(os.listdir(font_path)) == 0:
+        download_font(font_path)
+        
+        
 # ====================
 # helper functions		
 # ====================
 
 def download_file_from_google_drive(id, destination):
-	def get_confirm_token(response):
-		for key, value in response.cookies.items():
-			if key.startswith('download_warning'):
-				return value
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
 
-		return None
+        return None
 
-	def save_response_content(response, destination):
-		CHUNK_SIZE = 32768
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
 
-		with open(destination, "wb") as f:
-			for chunk in response.iter_content(CHUNK_SIZE):
-				if chunk: # filter out keep-alive new chunks
-					f.write(chunk)
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
 
-	URL = "https://docs.google.com/uc?export=download"
+    URL = "https://docs.google.com/uc?export=download"
 
-	session = requests.Session()
+    session = requests.Session()
 
-	response = session.get(URL, params = { 'id' : id }, stream = True)
-	token = get_confirm_token(response)
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
 
-	if token:
-		params = { 'id' : id, 'confirm' : token }
-		response = session.get(URL, params = params, stream = True)
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
 
-	save_response_content(response, destination)    
-		
+    save_response_content(response, destination)    
+        
